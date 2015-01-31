547f7012a62242281fa20f15bb6ae4bd8500fdbf
rotgut <rotgut@8c52f7fe-b2e1-4dd8-9948-e05ad466d5bd>
2015-01-31 22:02:33 +0000

    Summary: Organizational changes
    git-svn-id: svn://svn.code.sf.net/p/garnetlisp/svn/trunk@35 8c52f7fe-b2e1-4dd8-9948-e05ad466d5bd

--- src/aggregadgets/aggregadgets-compiler.lisp
+++ src/aggregadgets/aggregadgets-compiler.lisp
@@ -10,21 +10,6 @@
 ;;;
 ;;; $Id::                                                             $
 
-;;; Changes:
-;;; 10/2/03 RGA --- New compile/load protocol
-;;; 7/28/96 RGA --- changed to use garnet-compile/load
-;;; 14-May-93 Mickish     Added new files, split off from others
-;;; 20-Jan-92 Mickish     Removed make-package call
-;;; 26-Mar-91 Pervin      Load compiled files in Lucid.
-;;; 22-Mar-91 Pervin      Added setf of :garnet-modules, and provides at end.
-;;; 19-Mar-91 Pervin      Added aggregraphs.
-;;; 4-Mar-91 D'Souza      Removed nickname "MO" of Opal.
-;;; 5-Jun-90 Richardson   Added lispworks
-;;; 8-May-90 Dannenberg   Added new files
-;;; 16-Apr-90 Pervin      Changed #+explorer to #+(or allegro explorer)
-;;; 12-Apr-90 Mitchell    Added #+allegro (gc t)
-;;; 3/22/90 Robert Cook - Define the package "OPAL" for the TI Explorer
-
 (in-package "COMMON-LISP-USER")
 
 (defvar *debug-aggregadgets-mode* nil)
@@ -32,11 +17,11 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (proclaim
    (if *debug-aggregadgets-mode*
-       (and (boundp '*garnet-compile-debug-settings*)
-	    *garnet-compile-debug-settings*)
+       (and (boundp 'Garnet-Compile-Debug-Settings)
+	    Garnet-Compile-Debug-Settings)
        ;; Global default settings.
-       (and (boundp '*default-garnet-proclaim*) 
-	    *default-garnet-proclaim*))))
+       (and (boundp 'Default-Garnet-Proclaim) 
+	    Default-Garnet-Proclaim))))
 
 
 (Defvar Garnet-Aggregadgets-Files
