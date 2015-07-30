aab6b7a464c1d10a4bd2135e83206c0d6e784695
rpgoldman <rpgoldman@8c52f7fe-b2e1-4dd8-9948-e05ad466d5bd>
2015-07-30 21:20:51 +0000

    Summary: Added needed REQUIRE for SBCL.
    git-svn-id: svn://svn.code.sf.net/p/garnetlisp/svn/trunk@97 8c52f7fe-b2e1-4dd8-9948-e05ad466d5bd

--- src/utils/general.lisp
+++ src/utils/general.lisp
@@ -20,24 +20,27 @@
 
 (in-package "GARNET-UTILS")
 
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (require :sb-posix))
+
 (eval-when (:execute :load-toplevel :compile-toplevel)
   (export '(WHILE
-	    UNTIL
-	    TILL
-	    DO2LISTS
-	    DOLIST2
-	    M
-	    M1
-	    STRING+
-	    ADD-TO-LIST
-	    VERIFY-BINDING
-	    SAFE-FUNCTIONP
-	    SHELL-EXEC
-	    DIRECTORY-P
-	    PROBE-DIRECTORY
+            UNTIL
+            TILL
+            DO2LISTS
+            DOLIST2
+            M
+            M1
+            STRING+
+            ADD-TO-LIST
+            VERIFY-BINDING
+            SAFE-FUNCTIONP
+            SHELL-EXEC
+            DIRECTORY-P
+            PROBE-DIRECTORY
 
             PI/2  PI3/2  2PI -2PI SHORT-PI
-	    )))
+            )))
 
 
 (defconstant pi/2 (/ pi 2))
@@ -69,13 +72,13 @@
        (list2var  (gensym))
        (done-test (if either? 'and 'or)))
   `(let ((,list1var ,list1)
-	 (,list2var ,list2)
-	 ,var1 ,var2)
+         (,list2var ,list2)
+         ,var1 ,var2)
       (while (,done-test ,list1var ,list2var)
-	(setq ,var1 (car ,list1var))
-	(setq ,var2 (car ,list2var))
-	(setq ,list1var (cdr ,list1var))
-	(setq ,list2var (cdr ,list2var))
+        (setq ,var1 (car ,list1var))
+        (setq ,var2 (car ,list2var))
+        (setq ,list1var (cdr ,list1var))
+        (setq ,list2var (cdr ,list2var))
        ,@body))))
 
 (defmacro dolist2 ((var1 var2 list) &rest body)
@@ -103,32 +106,32 @@
      (add-to-list element list :before other-element)
      (add-to-list element list :after other-element)"
  (let  ((new-cons (list element))
-	result)
+        result)
    (if (null list)
        (setq result new-cons)
        (case where
-	 ((:head :front NIL)
-	  (setq result (cons element list)))
-	 ((:before)
-	  (if (eq (first list) locator)
-	      (setq result (cons element list))
-	      (do ((cons1 list (cdr cons1))
-		   (cons2 (cdr list) (cdr cons2)))
-		  ((null cons2))
-		(when (eq (first cons2) locator)
-		  (setf (cdr new-cons) cons2)
-		  (setf (cdr cons1) new-cons)
-		  (setq result list)
-		  (return)))))
-	 ((:after)
-	  (do ((cons1 list (cdr cons1))
-	       (cons2 (cdr list) (cdr cons2)))
-	      ((null cons1))
-	    (when (eq (first cons1) locator)
-	      (setf (cdr new-cons) cons2)
-	      (setf (cdr cons1) new-cons)
-	      (setq result list)
-	      (return))))))
+         ((:head :front NIL)
+          (setq result (cons element list)))
+         ((:before)
+          (if (eq (first list) locator)
+              (setq result (cons element list))
+              (do ((cons1 list (cdr cons1))
+                   (cons2 (cdr list) (cdr cons2)))
+                  ((null cons2))
+                (when (eq (first cons2) locator)
+                  (setf (cdr new-cons) cons2)
+                  (setf (cdr cons1) new-cons)
+                  (setq result list)
+                  (return)))))
+         ((:after)
+          (do ((cons1 list (cdr cons1))
+               (cons2 (cdr list) (cdr cons2)))
+              ((null cons1))
+            (when (eq (first cons1) locator)
+              (setf (cdr new-cons) cons2)
+              (setf (cdr cons1) new-cons)
+              (setq result list)
+              (return))))))
  (unless result
    (setf (cdr (last list)) new-cons)
    (setq result list))
@@ -145,31 +148,31 @@ symbol is bound.  Note: The suffix of the string is converted to all
 uppercase characters before checking if it is bound in the package."
   (let ((result-1 (verify-binding-aux string 0)))
     (if result-1
-	(let* ((colon-p (first result-1))
-	       (prefix (second result-1))
-	       (symbol-1 (values (read-from-string prefix)))
-	       (index (third result-1)))
-	  (if colon-p
-	      ;; Then symbol-1 indicates a package name
-	      (when (find-package symbol-1)
-		;; Then symbol-1 is a valid package name
-		(let ((result-2 (verify-binding-aux string (+ 1 index))))
-		  (when result-2
-		    ;; Then suffix indicates a var in the package symbol-1
-		    (let* ((suffix (string-upcase (second result-2)))
-			   (access-internal-p (fourth result-2)))
-		      (multiple-value-call
-			  #'(lambda (symbol-2 access)
-			      (if symbol-2
-				  (if (or (eq access :external)
-					  access-internal-p)
-				      ;; verify that symbol-2 is not a function
-				      (when (boundp symbol-2) 
-					(values (read-from-string string)))
-				      )))
-			(find-symbol suffix symbol-1))))))
-	      ;; Then symbol indicates a var in the working package
-	      (when (and (not (numberp symbol-1)) (boundp symbol-1)) symbol-1))))))
+        (let* ((colon-p (first result-1))
+               (prefix (second result-1))
+               (symbol-1 (values (read-from-string prefix)))
+               (index (third result-1)))
+          (if colon-p
+              ;; Then symbol-1 indicates a package name
+              (when (find-package symbol-1)
+                ;; Then symbol-1 is a valid package name
+                (let ((result-2 (verify-binding-aux string (+ 1 index))))
+                  (when result-2
+                    ;; Then suffix indicates a var in the package symbol-1
+                    (let* ((suffix (string-upcase (second result-2)))
+                           (access-internal-p (fourth result-2)))
+                      (multiple-value-call
+                          #'(lambda (symbol-2 access)
+                              (if symbol-2
+                                  (if (or (eq access :external)
+                                          access-internal-p)
+                                      ;; verify that symbol-2 is not a function
+                                      (when (boundp symbol-2) 
+                                        (values (read-from-string string)))
+                                      )))
+                        (find-symbol suffix symbol-1))))))
+              ;; Then symbol indicates a var in the working package
+              (when (and (not (numberp symbol-1)) (boundp symbol-1)) symbol-1))))))
 
 
 
@@ -181,20 +184,20 @@ or the symbol name if called where the first character is a colon."
     (when (> str-len start)
       ;; Skip second colon if there is a double colon between package and var
       (let ((access-internal-p (when (and (char= (char string start) #\:)
-					  (/= start 0))
-				 (incf start))))
-	;; Abort if a special character begins the string
-	(unless (or (char= (char string start) #\:)
-		    (char= (char string start) #\#))
-	  ;; Return the part of the string up to but not including the colon
-	  ;; and the index of the last character checked
-	  ;; FMG --- Just use lisp utilities to do this stuff.
-	  (let* ((colon (position #\: string :start start :test #'char=))
-		 (new-string (subseq string start colon)))
-	    (list (not (null colon)) 
-		  new-string
-		  (or colon (1- str-len)) 
-		  access-internal-p)))))))
+                                          (/= start 0))
+                                 (incf start))))
+        ;; Abort if a special character begins the string
+        (unless (or (char= (char string start) #\:)
+                    (char= (char string start) #\#))
+          ;; Return the part of the string up to but not including the colon
+          ;; and the index of the last character checked
+          ;; FMG --- Just use lisp utilities to do this stuff.
+          (let* ((colon (position #\: string :start start :test #'char=))
+                 (new-string (subseq string start colon)))
+            (list (not (null colon)) 
+                  new-string
+                  (or colon (1- str-len)) 
+                  access-internal-p)))))))
 
 ;;;
 ;;; (end) Verify-Binding
@@ -209,12 +212,12 @@ or the symbol name if called where the first character is a colon."
 ;; pathname facilities.
 (defun probe-directory (filename)
   #+clisp (let ((system::*error-handler*
-		 #'(lambda (&rest args)
-		     (declare (ignore args))
-		     (return-from probe-directory nil))
-		  ))
-	    ; The following causes an error if the directory does not exist
-	    (and (truename filename) t))
+                 #'(lambda (&rest args)
+                     (declare (ignore args))
+                     (return-from probe-directory nil))
+                  ))
+            ; The following causes an error if the directory does not exist
+            (and (truename filename) t))
 
   ;; Garnet under SBCL requires sb-posix.
   #+sbcl
@@ -225,8 +228,8 @@ or the symbol name if called where the first character is a colon."
   #+(or cmucl ccl)
   (let ((truename (probe-file filename)))
     (and truename
-	 (not (pathname-name truename))
-	 (not (pathname-type truename))))
+         (not (pathname-name truename))
+         (not (pathname-type truename))))
 
   #-(or clisp ccl cmucl sbcl allegro) (probe-file filename)
   )
@@ -236,40 +239,40 @@ or the symbol name if called where the first character is a colon."
   ;; by most of the lisp-specific commands.  Must close both streams.
   (multiple-value-bind (the-stream error-stream)
       #+allegro (excl:run-shell-command command :wait NIL :output :stream
-					:error-output :stream)
+                                        :error-output :stream)
       #+sbcl
       (let ((process
-	     (sb-ext:run-program "/bin/sh" (list "-c" command)
-				 :wait t :output :stream
-				 :error :stream)))
-	(values (sb-ext:process-output process)
-		(sb-ext:process-error process)))
+             (sb-ext:run-program "/bin/sh" (list "-c" command)
+                                 :wait t :output :stream
+                                 :error :stream)))
+        (values (sb-ext:process-output process)
+                (sb-ext:process-error process)))
       #+cmu
       (let ((p
-	     (ext:run-program "/bin/sh" (list "-c" command)
-			  :wait NIL :output :stream :error :stream)))
-	(values (ext:process-output p) (ext:process-error p)))
+             (ext:run-program "/bin/sh" (list "-c" command)
+                          :wait NIL :output :stream :error :stream)))
+        (values (ext:process-output p) (ext:process-error p)))
       #+ccl
       (let ((p
-	     (ccl:run-program "/bin/sh" (list "-c" command)
-			      :wait NIL :output :stream :error :stream)))
-	(values (ccl:external-process-output-stream p)
-		(ccl:external-process-error-stream p)))
+             (ccl:run-program "/bin/sh" (list "-c" command)
+                              :wait NIL :output :stream :error :stream)))
+        (values (ccl:external-process-output-stream p)
+                (ccl:external-process-error-stream p)))
       
       #-(or allegro cmu ccl sbcl)
       (error "Don't know how to execute shell functions in this lisp")
       
       (let ((output-string (make-array '(0)
-				       :element-type 'character
-				       :fill-pointer 0 :adjustable T)))
-	(do ((next-char (read-char the-stream NIL :eof)
-			(read-char the-stream NIL :eof)))
-	    ((eq next-char :eof)
-	     (close the-stream)
-	     (if (streamp error-stream) (close error-stream))
-	     #+allegro (system:os-wait))
-	  (vector-push-extend next-char output-string))
-	output-string)))
+                                       :element-type 'character
+                                       :fill-pointer 0 :adjustable T)))
+        (do ((next-char (read-char the-stream NIL :eof)
+                        (read-char the-stream NIL :eof)))
+            ((eq next-char :eof)
+             (close the-stream)
+             (if (streamp error-stream) (close error-stream))
+             #+allegro (system:os-wait))
+          (vector-push-extend next-char output-string))
+        output-string)))
 
 
 ;; If the -d test is true, shell-exec returns "1". Otherwise, it
@@ -287,9 +290,9 @@ or the symbol name if called where the first character is a colon."
   #-(or sbcl allegro ccl cmucl)
   ;; command-string is the string that's going to be executed.
   (let ((command-string
-	 (concatenate 'string "test -d " pathname " && echo 1")))
+         (concatenate 'string "test -d " pathname " && echo 1")))
     (unless (equal "" (shell-exec command-string))
-	    T))
+            T))
 )
 
 
@@ -304,21 +307,21 @@ or the symbol name if called where the first character is a colon."
   ;; #\Space, #\', #\<, #\>, #\$ etc. in a special way. This kind of quoting
   ;; should work unless the pathname contains #\Newline and we call csh.
   (flet ((shell-quote (string) ; surround a string by single quotes
-	   (let ((qchar nil) ; last quote character: nil or #\' or #\"
-		 (qstring (make-array 10 :element-type 'character
-				      :adjustable t :fill-pointer 0)))
-	     (map nil #'(lambda (c)
-			  (let ((q (if (eql c #\') #\" #\')))
-			    (unless (eql qchar q)
-			      (when qchar (vector-push-extend qchar qstring))
-			      (vector-push-extend (setq qchar q) qstring))
-			    (vector-push-extend c qstring)))
-		  string)
-	     (when qchar (vector-push-extend qchar qstring))
-	     qstring)))
+           (let ((qchar nil) ; last quote character: nil or #\' or #\"
+                 (qstring (make-array 10 :element-type 'character
+                                      :adjustable t :fill-pointer 0)))
+             (map nil #'(lambda (c)
+                          (let ((q (if (eql c #\') #\" #\')))
+                            (unless (eql qchar q)
+                              (when qchar (vector-push-extend qchar qstring))
+                              (vector-push-extend (setq qchar q) qstring))
+                            (vector-push-extend c qstring)))
+                  string)
+             (when qchar (vector-push-extend qchar qstring))
+             qstring)))
     ;; command-string is the string that's going to be executed.
     (let ((command-string
-	   (concatenate 'string "test -d " (shell-quote pathname) " && echo 1")))
+           (concatenate 'string "test -d " (shell-quote pathname) " && echo 1")))
       (unless (equal "" (shell-exec command-string))
-	T))))
-		   
+        T))))
+                   
