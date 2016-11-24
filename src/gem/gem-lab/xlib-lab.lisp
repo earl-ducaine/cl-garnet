d4526b4f2f929e52724aedefde1b9b06c2362e1c
Earl Ducaine <earl.ducaine-gmail-com>
2016-11-23 19:09:22 -0800

    Finish stipped down wrapper of xlib

--- src/gem/gem-lab/xlib-lab.lisp
+++ src/gem/gem-lab/xlib-lab.lisp
@@ -1,7 +1,7 @@
 (ql:quickload :clx)
 
 (defpackage :xlib-lab
-  (:use :common-lisp)
+  (:use :common-lisp :kr)
   (:export))
 
 (in-package :xlib-lab)
@@ -9,6 +9,7 @@
 (defparameter *system-initialized* nil)
 (defparameter *display* nil)
 (defparameter *screen* nil)
+(defparameter *root-window* nil)
 
 (defun init-xlib ()
   (setf *display* (xlib:open-default-display))
@@ -17,8 +18,21 @@
   (setf *screen* (first (xlib:display-roots *display*)))
   (unless *screen*
     (error "Unable to get valid default screen (display root)."))
+  (setf *root-window* (xlib:screen-root *screen*))
+  (unless *root-window*
+    (error "Unable to get valid root window of the screen."))
   (setf *system-initialized* t))
 
+(defparameter app-window nil)
+
+(defun create-window (&key x y width height)
+  (setf app-window
+	(xlib:create-window :parent *root-window*
+			    :x 10
+			    :y 15
+			    :width 200
+			    :height 250))
+  (xdisplay-force-output *display))
 
 ;; TODO -- can we assume:
 ;; #<XLIB:BITMAP-FORMAT unit 32 pad 32 LSB first>
@@ -99,3 +113,110 @@
   (unless *system-initialized*
     (init-xlib))
   (xlib:display-vendor *display*))
+
+;; TODO -- Doesn't seem like it would be useful (assigning resource
+;; ids)
+(defun display-xid ()
+  (unless *system-initialized*
+    (init-xlib))
+  (xlib:display-xid *display*))
+
+(defun display-finish-output ()
+  (unless *system-initialized*
+    (init-xlib))
+  (xlib:display-finish-output *display*))
+
+(defun display-force-output ()
+  (unless *system-initialized*
+    (init-xlib))
+  (xlib:display-force-output *display*))
+
+(defun close-display ()
+  (unless *system-initialized*
+    (init-xlib))
+  (xlib:close-display *display*))
+
+(defun screen-backing-stores ()
+  (unless *system-initialized*
+    (init-xlib))
+  (xlib:close-display *display*))
+
+(defun screen-backing-stores ()
+  (unless *system-initialized*
+    (init-xlib))
+  (xlib:screen-backing-stores *screen*))
+
+(defun screen-backing-stores ()
+  (unless *system-initialized*
+    (init-xlib))
+  (xlib:screen-backing-stores *screen*))
+
+(defun screen-black-pixel ()
+  (unless *system-initialized*
+    (init-xlib))
+  (xlib:screen-black-pixel *screen*))
+
+(defun screen-height-in-millimeters ()
+  (unless *system-initialized*
+    (init-xlib))
+  (xlib:screen-height-in-millimeters *screen*))
+
+(defun screen-width-in-millimeters ()
+  (unless *system-initialized*
+    (init-xlib))
+  (xlib:screen-width-in-millimeters *screen*))
+
+
+(defun screen-height ()
+  (unless *system-initialized*
+    (init-xlib))
+  (xlib:screen-height *screen*))
+
+(defun screen-width ()
+  (unless *system-initialized*
+    (init-xlib))
+  (xlib:screen-width *screen*))
+
+(defun screen-root ()
+  (unless *system-initialized*
+    (init-xlib))
+  (xlib:screen-root *screen*))
+
+(defun screen-root-depth ()
+  (unless *system-initialized*
+    (init-xlib))
+  (xlib:screen-root-depth *screen*))
+
+
+
+
+
+
+
+(defmacro with-xlib ((&key timeout inline)
+		     &body body)
+  ;; TODO -- It's not clear what this is useful for, nor, given some
+  ;; other issues, whether it works.
+  ;;
+  ;; This macro is for use in a multi-process environment.  It
+  ;; provides exclusive access to the local display object for
+  ;; multiple request generation.  It need not provide immediate
+  ;; exclusive access for replies; that is, if another process is
+  ;; waiting for a reply (while not in a with-display), then
+  ;; synchronization need not (but can) occur immediately.  Except
+  ;; where noted, all routines effectively contain an implicit
+  ;; with-display where needed, so that correct synchronization is
+  ;; always provided at the interface level on a per-call basis.
+  ;; Nested uses of this macro will work correctly.  This macro does
+  ;; not prevent concurrent event processing; see with-event-queue.
+  `(progn
+     (unless *system-initialized*
+       (init-xlib))
+     (xlib:with-display (,*display*
+			  ,@(and timeout `(:timeout ,timeout))
+			  ,@(and inline `(:inline ,inline)))
+	,@body)))
+
+
+
+;; look for the wmDeleteMessage message
