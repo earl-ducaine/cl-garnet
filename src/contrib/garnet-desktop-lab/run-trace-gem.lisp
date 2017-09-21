220bc85217088930a5f335c7bd278f2cc6cf1d32
Earl Ducaine <earl.ducaine-gmail-com>
2017-09-21 12:50:31 -0700

    Adding gtk themed menu bar

--- src/contrib/garnet-desktop-lab/run-trace-gem.lisp
+++ src/contrib/garnet-desktop-lab/run-trace-gem.lisp
@@ -1,30 +1,19 @@
 
 (in-package :gem)
-(trace-gem :x)
-(defparameter demo-3d-do-go (demo-3d:do-go))
 
 (defparameter *standard-output-bak* *standard-output*)
 (defparameter *error-output-bak* *error-output*)
 (defparameter *trace-output-bak* *trace-output*)
 
+(setf *trace-output* (make-string-output-stream))
 
-
-
-(defparameter *standard-output* (make-string-output-stream))
-(defparameter *error-output* (make-string-output-stream))
-(defparameter *trace-output* (make-string-output-stream))
-)
-
-*trace-output*
+(trace-gem :x)
 
 ;; (defun print-stuff (x y)
 ;;   (format t "standard output ~a" x)
 ;;   (format *error-output* "error output ~a" y)
 ;;   (+ x y))
 
-(defun capture (x y)
-  (let ((*standard-output* (make-string-output-stream))
-        (*error-output* (make-string-output-stream)))
-    (values (demo-3d:do-go)
-            (get-output-stream-string *standard-output*)
-            (get-output-stream-string *error-output*))))
+(defun run-do-go ()
+  (demo-3d:do-go)
+  (get-output-stream-string *trace-output*))
