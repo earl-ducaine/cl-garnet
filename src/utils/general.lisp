6e4af7a03bc4ae8541dad7754bfa82243a7ce0e8
Earl DuCaine <earl.ducaine+gmail.com>
2020-02-10 10:05:29 -0800

    continuing

--- src/utils/general.lisp
+++ src/utils/general.lisp
@@ -1,15 +1,3 @@
-;;; -*- Mode: LISP; Syntax: Common-Lisp; Package: GARNET-UTILS; Base: 10 -*-
-
-
-;; The Garnet User Interface Development Environment.
-;;
-;; This code was written as part of the Garnet project at Carnegie
-;; Mellon University, and has been placed in the public domain.
-;;
-;; by David S. Kosbie
-
-;; Host of Lisp utilities used by other Garnet code.
-
 (in-package :garnet-utils)
 
 (defvar *debug-utils-mode* t)
@@ -38,37 +26,7 @@
   `(loop ,@body
       (when ,test (return))))
 
-(defmacro do2lists ((var1 list1 var2 list2 &key either?) &rest body)
- (let ((list1var  (gensym))
-       (list2var  (gensym))
-       (done-test (if either? 'and 'or)))
-  `(let ((,list1var ,list1)
-         (,list2var ,list2)
-         ,var1 ,var2)
-      (while (,done-test ,list1var ,list2var)
-        (setq ,var1 (car ,list1var))
-        (setq ,var2 (car ,list2var))
-        (setq ,list1var (cdr ,list1var))
-        (setq ,list2var (cdr ,list2var))
-       ,@body))))
-
-(defmacro dolist2 ((var1 var2 list) &rest body)
-  (let ((listvar (gensym)))
-  `(let ((,listvar ,list) ,var1 ,var2)
-     (while ,listvar
-       (setq ,var1 (car ,listvar))
-       (setq ,var2 (cadr ,listvar))
-       (setq ,listvar (cddr ,listvar))
-       ,@body))))
-
-(defmacro m (s-expr)
-  `(pprint (macroexpand (quote ,s-expr))))
-
-(defmacro m1 (s-expr)
-  `(pprint (macroexpand-1 (quote ,s-expr))))
-
 (defmacro string+ (&rest args) `(concatenate 'string ,@args))
-
 
 (defun safe-functionp (fn)
   (or (functionp fn)
