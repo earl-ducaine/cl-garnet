f046f2e27828d0d74621454594ba6d9af7543bd7
Earl DuCaine <earl.ducaine+gmail.com>
2020-02-10 14:31:29 -0800

    continuing

--- multi-garnet/scatterplot-alt.lisp
+++ multi-garnet/scatterplot-alt.lisp
@@ -1,20 +1,25 @@
 (in-package :kr)
 
-;; (create-instance '*axis-rectangle* Opal::Rectangle
-;;   (:left-cn (m-constraint :max (box left) (setf left (first box))))
-;;   (:top-cn (m-constraint :max (box top) (setf top (second box))))
-;;   (:width-cn (m-constraint :max (box width) (setf width (third box))))
-;;   (:height-cn (m-constraint :max (box height) (setf height (fourth box)))))
+;; (CREATE-SCHEMA '*AXIS-RECTANGLE* :GENERATE-INSTANCE (:IS-A OPAL::RECTANGLE)
+;;                 (:LEFT-CN
+;;                  (M-CONSTRAINT :MAX (BOX LEFT) (SETF LEFT (FIRST BOX))))
+;;                 (:TOP-CN (M-CONSTRAINT :MAX (BOX TOP) (SETF TOP (SECOND BOX))))
+;;                 (:WIDTH-CN
+;;                  (M-CONSTRAINT :MAX (BOX WIDTH) (SETF WIDTH (THIRD BOX))))
+;;                 (:HEIGHT-CN
+;;                  (M-CONSTRAINT :MAX (BOX HEIGHT) (SETF HEIGHT (FOURTH BOX)))))
 
-
-(CREATE-SCHEMA '*AXIS-RECTANGLE* :GENERATE-INSTANCE (:IS-A OPAL::RECTANGLE)
-                (:LEFT-CN
-                 (M-CONSTRAINT :MAX (BOX LEFT) (SETF LEFT (FIRST BOX))))
-                (:TOP-CN (M-CONSTRAINT :MAX (BOX TOP) (SETF TOP (SECOND BOX))))
-                (:WIDTH-CN
-                 (M-CONSTRAINT :MAX (BOX WIDTH) (SETF WIDTH (THIRD BOX))))
-                (:HEIGHT-CN
-                 (M-CONSTRAINT :MAX (BOX HEIGHT) (SETF HEIGHT (FOURTH BOX)))))
+(DO-SCHEMA-BODY (MAKE-A-NEW-SCHEMA '*AXIS-RECTANGLE*) OPAL::RECTANGLE T T NIL
+                NIL
+                (CONS :HEIGHT-CN
+                      (M-CONSTRAINT :MAX (BOX HEIGHT)
+                                    (SETF HEIGHT (FOURTH BOX))))
+                (CONS :WIDTH-CN
+                      (M-CONSTRAINT :MAX (BOX WIDTH) (SETF WIDTH (THIRD BOX))))
+                (CONS :TOP-CN
+                      (M-CONSTRAINT :MAX (BOX TOP) (SETF TOP (SECOND BOX))))
+                (CONS :LEFT-CN
+                      (M-CONSTRAINT :MAX (BOX LEFT) (SETF LEFT (FIRST BOX)))))
 
 (DO-SCHEMA-BODY (MAKE-A-NEW-SCHEMA '*V-AXIS*) OPAL:AGGREGADGET T T NIL NIL
                 (CONS :PARTS `((:MAIN ,*AXIS-RECTANGLE*))))
