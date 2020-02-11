58755b1e6fb15a556c4ba884bcccd3671753965a
Earl DuCaine <earl.ducaine+gmail.com>
2020-02-11 11:56:27 -0800

    continuing

--- src/opal/macros.lisp
+++ src/opal/macros.lisp
@@ -2,28 +2,12 @@
 
 (in-package :kr)
 
-(declaim (notinline bottom))
-(declaim (notinline right))
-
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (export '(gv-fixnum gvl-fixnum g-value-fixnum)))
+  (export '(gv-fixnum g-value-fixnum)))
 
 ;; FMG Some common idioms supporting optimization.
 (defmacro gv-fixnum (object &rest slots)
   `(the fixnum (gv ,object ,@slots)))
 
-(defmacro gvl-fixnum (&rest slots)
-  `(the fixnum (gvl ,@slots)))
-
 (defmacro g-value-fixnum (object &rest slots)
   `(the fixnum (g-value ,object ,@slots)))
-
-
-
-(in-package "OPAL")
-
-
-;; ;; Wrappers for KR-SEND.
-;; (defmacro add-component (schema &rest args)
-;;   `(let ((the-schema ,schema))
-;;      (kr-send the-schema :add-component the-schema ,@args)))
