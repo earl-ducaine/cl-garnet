4dca9e9d72626597ebbf0290d4bff49fdb6266c0
Earl DuCaine <earl.ducaine+gmail.com>
2020-02-21 20:11:07 -0800

    continuing

--- kr-macros.lisp
+++ kr-macros.lisp
@@ -16,71 +16,14 @@
 (defparameter iterate-slot-value-entry nil
   "Ugly")
 
-(defun kr-init-method-hook (schema)
-  (maphash
-   #'(lambda (iterate-ignored-slot-name iterate-slot-value-entry)
-       (declare (ignore iterate-ignored-slot-name))
-       (let ((slot (sl-name iterate-slot-value-entry)))
-	 (setf (gethash slot (schema-bins schema))
-	       (make-sl :name slot :bits 0))))
-   (schema-bins (car (sl-value (gethash :is-a (schema-bins schema))))))
-  (locally
-      (declare (optimize (speed 3) (safety 0) (space 0) (debug 0)))
-    (maphash
-     #'(lambda (iterate-ignored-slot-name iterate-slot-value-entry)
-	 (declare (ignore iterate-ignored-slot-name))
-	 (let* ((cn
-		 (sl-value
-		  (gethash (sl-name iterate-slot-value-entry)
-			   (schema-bins schema)))))
-	   (when (and (sb-constraint-p cn)
-		      (getf (sb-constraint-other-slots cn)
-			    :mg-connection nil))
-	     (setf (getf (sb-constraint-other-slots cn) :mg-os nil)
-		   (cons schema (sl-name iterate-slot-value-entry)))
-	     (let* ((constraint-slot
-		     (loop for path in (getf (sb-constraint-other-slots cn)
-					     :mg-variable-paths nil)
-			collect (loop for (slot next-slot) on path
-				   do (return
-					(cons
-					 (car
-					  (getf
-					   (sb-constraint-other-slots cn)
-					   :mg-os nil))
-					 slot))))))
-	       (let ((new1
-		      (loop for var-os in constraint-slot
-			 collect (let ((obj (car var-os))
-				       (slot (cdr var-os)))
-				   (setf (gethash slot (schema-bins obj))
-					 (make-sl :name slot :bits 0))
-				   (make-sl)))))
-		 (setf (sb-constraint-variables cn) new)))
-	     (sb-constraint-set-slot-fn cn)
-	     (setf (getf (sb-constraint-other-slots cn)
-			 :mg-connection nil)
-		   :connected))))
-     (schema-bins schema))))
 
-(defmacro create-mg-constraint (schema)
-  `(let* ((cn (make-sb-constraint
-	      :other-slots
-	      (list :mg-connection :unconnected
-		    :mg-variable-paths (list '(:box) (list (gensym)))))))
-    (let* ((symbol (gensym))
-	   (sl (make-sl :name symbol
-			:value cn)))
-      (setf (gethash symbol (schema-bins ,schema)) sl)
-      sl)))
+(defvar *graphical-object*
+  (make-schema :bins (make-hash-table :test #'eq)))
 
-(defvar *graphical-object* (make-schema :bins (make-hash-table :test #'eq)))
 (defvar *rectangle* (make-schema :bins (make-hash-table :test #'eq)))
 
 (setf (gethash :is-a (schema-bins *graphical-object*))
       (make-sl :name :is-a :value nil))
-
-
 
 (setf (gethash :filling-style (schema-bins *graphical-object*))
       (make-sl :name :filling-style
@@ -152,9 +95,52 @@
 			:bits bits)))))
  (schema-bins *rectangle*))
 
+;;(kr-init-method-hook *axis-rectangle*)
 
-;; Note, eliminating this and change the corresponding call to
-;; kr-init-method-hook caused the memorry error #0x rather than #5x
-(setf (symbol-function 'kr-init-method) #'kr-init-method-hook)
 
-(kr-init-method-hook *axis-rectangle*)
+(maphash
+ #'(lambda (iterate-ignored-slot-name iterate-slot-value-entry)
+     (declare (ignore iterate-ignored-slot-name))
+     (let ((slot (sl-name iterate-slot-value-entry)))
+       (setf (gethash slot (schema-bins *axis-rectangle*))
+	     (make-sl :name slot :bits 0))))
+ (schema-bins (car (sl-value (gethash :is-a (schema-bins *axis-rectangle*))))))
+
+(locally
+    (declare (optimize (speed 3) (safety 0) (space 0) (debug 0)))
+  (maphash
+   #'(lambda (iterate-ignored-slot-name iterate-slot-value-entry)
+       (declare (ignore iterate-ignored-slot-name))
+       (let* ((cn
+	       (sl-value
+		(gethash (sl-name iterate-slot-value-entry)
+			 (schema-bins *axis-rectangle*)))))
+	 (when (and (sb-constraint-p cn)
+		    (getf (sb-constraint-other-slots cn)
+			  :mg-connection nil))
+	   (setf (getf (sb-constraint-other-slots cn) :mg-os nil)
+		 (cons *axis-rectangle* (sl-name iterate-slot-value-entry)))
+	   (let* ((constraint-slot
+		   (loop for path in (getf (sb-constraint-other-slots cn)
+					   :mg-variable-paths nil)
+		      collect (loop for (slot next-slot) on path
+				 do (return
+				      (cons
+				       (car
+					(getf
+					 (sb-constraint-other-slots cn)
+					 :mg-os nil))
+				       slot))))))
+	     (let ((new1
+		    (loop for var-os in constraint-slot
+		       collect (let ((obj (car var-os))
+				     (slot (cdr var-os)))
+				 (setf (gethash slot (schema-bins obj))
+				       (make-sl :name slot :bits 0))
+				 (make-sl)))))
+	       (setf (sb-constraint-variables cn) new)))
+	   (sb-constraint-set-slot-fn cn)
+	   (setf (getf (sb-constraint-other-slots cn)
+		       :mg-connection nil)
+		 :connected))))
+   (schema-bins *axis-rectangle*)))
