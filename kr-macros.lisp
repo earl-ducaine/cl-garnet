1753a2aa378aa471a281c4b77b821c45765c6d1c
Earl DuCaine <earl.ducaine+gmail.com>
2020-02-23 12:42:58 -0800

    continuing

--- kr-macros.lisp
+++ kr-macros.lisp
@@ -1,19 +1,19 @@
 
-(defstruct sl
+(defstruct s
   name
   value)
 
 (defparameter *hash-table* (make-hash-table :test #'eq))
 
-(defparameter *constraint-1* (make-sl :value :slot-1-box))
-(defparameter *constraint-2* (make-sl :value :slot-2-box))
-(defparameter *constraint-3* (make-sl :value :slot-3-box))
-(defparameter *constraint-4* (make-sl :value :slot-4-box))
+(defparameter *constraint-1* (make-s :value :slot-1-box))
+(defparameter *constraint-2* (make-s :value :slot-2-box))
+(defparameter *constraint-3* (make-s :value :slot-3-box))
+(defparameter *constraint-4* (make-s :value :slot-4-box))
 
-(defparameter *slot-1* (make-sl :name :a :value *constraint-1*))
-(defparameter *slot-2* (make-sl :name :b :value *constraint-2*))
-(defparameter *slot-3* (make-sl :name :c :value *constraint-3*))
-(defparameter *slot-4* (make-sl :name :d :value *constraint-4*))
+(defparameter *slot-1* (make-s :name :a :value *constraint-1*))
+(defparameter *slot-2* (make-s :name :b :value *constraint-2*))
+(defparameter *slot-3* (make-s :name :c :value *constraint-3*))
+(defparameter *slot-4* (make-s :name :d :value *constraint-4*))
 
 (setf (gethash :a *hash-table*) *slot-1*)
 (setf (gethash :b *hash-table*) *slot-2*)
@@ -33,8 +33,8 @@
     (maphash
      #'(lambda (iterate-ignored-slot-name slot-value-entry)
 	 (declare (ignore iterate-ignored-slot-name))
-	 (let ((slot (sl-value (sl-value
-						 (gethash (sl-name slot-value-entry)
+	 (let ((slot (s-value (s-value
+						 (gethash (s-name slot-value-entry)
 							  *hash-table*)))))
 	   (setf (gethash :box *hash-table*) nil)
 	   (setf (gethash slot *hash-table*) nil)))
