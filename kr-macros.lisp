8d51041b9dc21734707ba97dfdc7ab30e8a068b4
Earl DuCaine <earl.ducaine+gmail.com>
2020-02-23 12:02:54 -0800

    continuing

--- kr-macros.lisp
+++ kr-macros.lisp
@@ -11,65 +11,37 @@
   other-slots
   set-slot-fn)
 
-;; Hash tables
 (defparameter *hash-table* (make-hash-table :test #'eq))
 
-
-;; Connections
 (defparameter *constraint-1*
-  (make-sb-constraint :other-slots '(:mg-connection :unconnected
-				     :mg-variable-paths
-				     ((:box) (:axis-rectangle-slot-1-box)))))
+  (make-sb-constraint :other-slots nil))
 
 (defparameter *constraint-2*
-  (make-sb-constraint :other-slots '(:mg-connection :unconnected
-				     :mg-variable-paths
-				     ((:box) (:axis-rectangle-slot-2-box)))))
+  (make-sb-constraint
+   :other-slots '(:mg-connection :unconnected
+		  :mg-variable-paths ((:box) (:slot-2-box)))))
 
 (defparameter *constraint-3*
-  (make-sb-constraint :other-slots '(:mg-connection :unconnected
-				     :mg-variable-paths
-				     ((:box) (:axis-rectangle-slot-3-box)))))
+  (make-sb-constraint
+   :other-slots '(:mg-connection :unconnected
+		  :mg-variable-paths ((:box) (:slot-3-box)))))
 
 (defparameter *constraint-4*
-  (make-sb-constraint :other-slots '(:mg-connection :unconnected
-				     :mg-variable-paths
-				     ((:box) (:axis-rectangle-slot-4-box)))))
-
-;; Slots axis rectangle
-(defparameter *axis-rectangle-is-a-sl*
-  (make-sl :name :is-a))
-
-(defparameter *axis-rectangle-slot-1*
-  (make-sl :name :axis-rectangle-slot-1
-		 :value *constraint-1*))
-
-(defparameter *axis-rectangle-slot-2*
-  (make-sl :name :axis-rectangle-slot-2
-	       :value *constraint-2*))
-
-(defparameter *axis-rectangle-slot-3*
-  (make-sl :name :axis-rectangle-slot-3
-	       :value *constraint-3*))
-
-(defparameter *axis-rectangle-slot-4*
-  (make-sl :name :axis-rectangle-slot-4
-	   :value *constraint-4* ))
+  (make-sb-constraint
+   :other-slots '(:mg-connection :unconnected
+		  :mg-variable-paths ((:box) (:slot-4-box)))))
 
 
+(defparameter *slot-1* (make-sl :name :slot-1 :value *constraint-1*))
+(defparameter *slot-2* (make-sl :name :slot-2 :value *constraint-2*))
+(defparameter *slot-3* (make-sl :name :slot-3 :value *constraint-3*))
+(defparameter *slot-4* (make-sl :name :slot-4 :value *constraint-4* ))
 
 
-(setf (gethash :axis-rectangle-slot-1 *hash-table*)
-	*axis-rectangle-slot-1*)
-
-(setf (gethash :axis-rectangle-slot-2 *hash-table*)
-      *axis-rectangle-slot-2*)
-
-(setf (gethash :axis-rectangle-slot-3 *hash-table*)
-      *axis-rectangle-slot-3*)
-
-(setf (gethash :axis-rectangle-slot-4 *hash-table*)
-      *axis-rectangle-slot-4*)
+(setf (gethash :slot-1 *hash-table*) *slot-1*)
+(setf (gethash :slot-2 *hash-table*) *slot-2*)
+(setf (gethash :slot-3 *hash-table*) *slot-3*)
+(setf (gethash :slot-4 *hash-table*) *slot-4*)
 
 (setf (gethash :1 *hash-table*) t)
 (setf (gethash :2 *hash-table*) t)
@@ -77,8 +49,6 @@
 (setf (gethash :4 *hash-table*) t)
 (setf (gethash :5 *hash-table*) t)
 (setf (gethash :6 *hash-table*) t)
-
-
 
 (defun create-error ()
   (locally
@@ -89,11 +59,11 @@
 	 (let* ((cn  (sl-value
 		      (gethash (sl-name iterate-slot-value-entry)
 			       *hash-table*))))
-	   (format t "cn: ~s~%iterate-slot-value-entry: ~s~%" cn iterate-slot-value-entry)
-	   (when (sb-constraint-p cn)
+	   (format t "cn: ~s~%iterate-slot-value-entry: ~s~%"
+		   cn iterate-slot-value-entry)
 	     (let ((slots (getf (sb-constraint-other-slots cn) :mg-variable-paths)))
 	     (setf (gethash (car slots) *hash-table*) nil)
-	     (setf (gethash (cadr slots) *hash-table*) nil))
+	     (setf (gethash (cadr slots) *hash-table*) nil)
 	   (setf (getf (sb-constraint-other-slots cn)
 		       :mg-connection nil)
 		 :connected))))
