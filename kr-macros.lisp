5e0f65386d558db94ef35a273a723f6c0a62c7a7
Earl DuCaine <earl.ducaine+gmail.com>
2020-02-23 11:26:47 -0800

    continuing

--- kr-macros.lisp
+++ kr-macros.lisp
@@ -4,84 +4,72 @@
 
 (defstruct sl
   name
-  value
-  (bits 0 :type fixnum))
+  value)
 
 (defstruct sb-constraint
   variables
   other-slots
   set-slot-fn)
 
-(defparameter iterate-slot-value-entry nil
-  "Ugly")
-
 ;; Hash tables
-(defparameter *graphical-object-hash-table* (make-hash-table :test #'eq))
-(defparameter *rectangle-hash-table* (make-hash-table :test #'eq))
 (defparameter *axis-rectangle-hash-table* (make-hash-table :test #'eq))
 
-;; Schemas
-(defparameter *graphical-object* (make-schema :name :graphical-object))
-(defparameter *rectangle* (make-schema :name :rectangle ))
-(defparameter *axis-rectangle* (make-schema :name :axis-rectangle))
 
 ;; Connections
-
-(defparameter *axis-rectangle-hash-table-cn-1*
+(defparameter *axis-rectangle-cn-1*
   (make-sb-constraint :other-slots '(:mg-connection :unconnected
 				     :mg-variable-paths
 				     ((:box) (:axis-rectangle-slot-1-box)))))
 
-(defparameter *axis-rectangle-hash-table-cn-2*
+(defparameter *axis-rectangle-cn-2*
   (make-sb-constraint :other-slots '(:mg-connection :unconnected
 				     :mg-variable-paths
 				     ((:box) (:axis-rectangle-slot-2-box)))))
 
-(defparameter *axis-rectangle-hash-table-cn-3*
+(defparameter *axis-rectangle-cn-3*
   (make-sb-constraint :other-slots '(:mg-connection :unconnected
 				     :mg-variable-paths
 				     ((:box) (:axis-rectangle-slot-3-box)))))
 
-(defparameter *axis-rectangle-hash-table-cn-4*
+(defparameter *axis-rectangle-cn-4*
   (make-sb-constraint :other-slots '(:mg-connection :unconnected
 				     :mg-variable-paths
 				     ((:box) (:axis-rectangle-slot-4-box)))))
 
 ;; Slots axis rectangle
-
 (defparameter *axis-rectangle-is-a-sl*
-  (make-sl :name :is-a :bits 0))
+  (make-sl :name :is-a))
 
 (defparameter *axis-rectangle-slot-1*
   (make-sl :name :axis-rectangle-slot-1
-		 :value *axis-rectangle-hash-table-cn-1*))
+		 :value *axis-rectangle-cn-1*))
 
 (defparameter *axis-rectangle-slot-2*
   (make-sl :name :axis-rectangle-slot-2
-	       :value *axis-rectangle-hash-table-cn-2*))
+	       :value *axis-rectangle-cn-2*))
 
 (defparameter *axis-rectangle-slot-3*
   (make-sl :name :axis-rectangle-slot-3
-	       :value *axis-rectangle-hash-table-cn-3*))
+	       :value *axis-rectangle-cn-3*))
 
 (defparameter *axis-rectangle-slot-4*
   (make-sl :name :axis-rectangle-slot-4
-	   :value *axis-rectangle-hash-table-cn-4*))
+	   :value *axis-rectangle-cn-4*))
 
 (defparameter *axis-rectangle-fast-redraw-p-sl*
-  (make-sl :name :fast-redraw-p :bits 0))
+  (make-sl :name :fast-redraw-p))
 
 (defparameter *axis-rectangle-filling-style-sl*
-  (make-sl :name :filling-style :bits 0))
+  (make-sl :name :filling-style))
 
 (defparameter *axis-rectangle-line-style-sl*
-  (make-sl :name :line-style :bits 0))
+  (make-sl :name :line-style))
 
 (defparameter *axis-rectangle-is-a-inv-sl*
-  (make-sl :name :is-a-inv :bits 0))
+  (make-sl :name :is-a-inv))
 
 (defparameter *axis-rectangle-nil-sl*
-  (make-sl :name nil :bits 0))
+  (make-sl :name nil))
 
 
 
