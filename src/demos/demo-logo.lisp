0cf01cd21b2468d38f0a2cb6925378801fcc32aa
Earl DuCaine <earl.ducaine+gmail.com>
2020-05-11 01:27:32 -0700

    Fix behaviour of animated logo
    - Introduced sleep (the fundamental mechanism of Garnet animation) to
      make angled text visible again.
    
    - Added constraints to top level window to causing it to resize
      appropriately when its contents run off the display.
    
    - Modernized format of some of the lisp code, especialy mixed-case -->
      all-lower-case, *asterisks* around special variables.

--- src/demos/demo-logo.lisp
+++ src/demos/demo-logo.lisp
@@ -14,55 +14,55 @@
 
 (in-package :DEMO-LOGO)
 
-(declaim (special PANTONE192 PANTONE193 PANTONE194 PANTONE196
-		  MYCOLOR1 MYCOLOR2 MYCOLOR3 MYCOLOR4 MYCOLOR5
-		  PANTONE192-FILL PANTONE193-FILL PANTONE194-FILL
-		  PANTONE196-FILL MYCOLOR1-FILL MYCOLOR2-FILL
-		  MYCOLOR3-FILL MYCOLOR4-FILL MYCOLOR5-FILL FADER))
+(declaim (special pantone192 pantone193 pantone194 pantone196
+		  mycolor1 mycolor2 mycolor3 mycolor4 mycolor5
+		  pantone192-fill pantone193-fill pantone194-fill
+		  pantone196-fill mycolor1-fill mycolor2-fill
+		  mycolor3-fill mycolor4-fill mycolor5-fill fader))
 
 (create-instance 'pantone192 opal:color
-  (:BLUE  0.35000002)
-  (:GREEN  0.060000002)
-  (:RED  1))
+  (:blue  0.35000002)
+  (:green  0.060000002)
+  (:red  1))
 
 (create-instance 'pantone193 opal:color
-  (:BLUE  0.23500001)
-  (:GREEN  0)
-  (:RED  0.885))
+  (:blue  0.23500001)
+  (:green  0)
+  (:red  0.885))
 
 (create-instance 'pantone194 opal:color
-  (:BLUE  0.10)
-  (:GREEN  0)
-  (:RED  0.66))
+  (:blue  0.10)
+  (:green  0)
+  (:red  0.66))
 
 (create-instance 'pantone196 opal:color
-  (:BLUE  0.94)
-  (:GREEN  0)
-  (:RED  1))
+  (:blue  0.94)
+  (:green  0)
+  (:red  1))
 
 (create-instance 'mycolor1 opal:color
-  (:BLUE  0.27500004)
-  (:GREEN  0.014999986)
-  (:RED  0.925))
+  (:blue  0.27500004)
+  (:green  0.014999986)
+  (:red  0.925))
 
 (create-instance 'mycolor2 opal:color
-  (:BLUE  0.7)
-  (:GREEN  0.7)
-  (:RED  1.0))
+  (:blue  0.7)
+  (:green  0.7)
+  (:red  1.0))
 
 (create-instance 'mycolor3 opal:color
-  (:BLUE  0.22)
-  (:GREEN  0)
-  (:RED  0.82))
+  (:blue  0.22)
+  (:green  0)
+  (:red  0.82))
 
 (create-instance 'mycolor4 opal:color
-  (:BLUE  0.84)
-  (:GREEN  0.84)
-  (:RED  1.0))
+  (:blue  0.84)
+  (:green  0.84)
+  (:red  1.0))
 (create-instance 'mycolor5 opal:color
-  (:BLUE  0.5)
-  (:GREEN  0.5)
-  (:RED  1.0))
+  (:blue  0.5)
+  (:green  0.5)
+  (:red  1.0))
 
 
 (create-instance 'pantone192-fill opal:filling-style
@@ -84,76 +84,56 @@
 (create-instance 'mycolor5-fill opal:filling-style
 		 (:foreground-color mycolor5))
 
-(defparameter new-point-list
- `(
-   (,pantone192-fill
-    (83 41 104 55 139 194 126 217 56 234 34 220 0 81 13 59 83 41) ;; L1
-    )
-   (,pantone194-fill
-    (104 55 93 68 85 70 83 62 83 41 104 55) ;; L2
-    )
-   (,opal:no-fill
-    (0 81 17 88 24 86 22 77 13 59 0 81) ;; L3
-    )
-   (,pantone194-fill
-    (34 220 46 207 53 205 56 213 56 234 34 220) ;; L4
-    )
-   (,pantone194-fill
-    (58 196 53 205 24 86 32 91 58 196) ;; L5
-    )
-   (,opal:no-fill
-    (139 194 122 187 115 189 116 198 126 217 139 194) ;; L6
-    )
-   (,mycolor2-fill
-    (107 184 115 189 85 70 81 79 107 184) ;; L7
-    )
-   (,mycolor1-fill
-    (104 55 139 194 122 187 93 68 104 55) ;; L8
-    )
-   (,pantone193-fill
-    (0 81 34 220 46 207 17 88 0 81) ;; L9
-    )
-   (,pantone193-fill
-    (83 41 83 62 22 77 13 59 83 41) ;; L10
-    )
-   (,opal:no-fill
-    (126 217 116 198 56 213 56 234 126 217) ;; L11
-    )
-   (,mycolor4-fill
-    (85 70 24 86 32 91 81 79 85 70) ;; L12
-    )
-   (,mycolor5-fill
-    (115 189 53 205 58 196 107 184 115 189) ;; L13
-    )
-   (,mycolor5-fill
-    (115 189 122 187 93 68 85 70 115 189) ;; L14
-    )
-   (,opal:white-fill
-    (93 68 83 62 85 70 93 68) ;; L15
-    )
-   (,opal:white-fill
-    (84 42 55 32 82 38 73 31 82 36 78 0 86 38 92 24
-     88 40 114 44 88 42 92 68 84 42) ;; L16, star
-    )
-   (,pantone193-fill
-    (53 205 115 189 116 198 56 213 53 205) ;; L17
-    )
-   (,mycolor1-fill
-    (46 207 53 205 24 86 17 88 46 207) ;; L18
-    )
-   (,mycolor1-fill
-    (24 86 22 77 83 62 85 70 24 86) ;; L19
-    )
-   ))
+(defparameter *new-point-list*
+  `(
+    ;; L1
+    (,pantone192-fill (83 41 104 55 139 194 126 217 56 234 34 220 0 81 13 59
+			  83 41))
+    ;; L2
+    (,pantone194-fill (104 55 93 68 85 70 83 62 83 41 104 55))
+    ;; L3
+    (,opal:no-fill (0 81 17 88 24 86 22 77 13 59 0 81))
+    ;; L4
+    (,pantone194-fill (34 220 46 207 53 205 56 213 56 234 34 220))
+    ;; L5
+    (,pantone194-fill (58 196 53 205 24 86 32 91 58 196))
+    ;; L6
+    (,opal:no-fill (139 194 122 187 115 189 116 198 126 217 139 194))
+    ;; L7
+    (,mycolor2-fill (107 184 115 189 85 70 81 79 107 184))
+    ;; L8
+    (,mycolor1-fill (104 55 139 194 122 187 93 68 104 55))
+    ;; L9
+    (,pantone193-fill (0 81 34 220 46 207 17 88 0 81))
+    ;; L10
+    (,pantone193-fill (83 41 83 62 22 77 13 59 83 41))
+    ;; L11
+    (,opal:no-fill (126 217 116 198 56 213 56 234 126 217))
+    ;; L12
+    (,mycolor4-fill (85 70 24 86 32 91 81 79 85 70))
+    ;; L13
+    (,mycolor5-fill (115 189 53 205 58 196 107 184 115 189))
+    ;; L14
+    (,mycolor5-fill (115 189 122 187 93 68 85 70 115 189))
+    ;; L15
+    (,opal:white-fill (93 68 83 62 85 70 93 68))
+    ;; L16, star
+    (,opal:white-fill (84 42 55 32 82 38 73 31 82 36 78 0 86 38 92 24 88 40
+			  114 44 88 42 92 68 84 42))
+    ;; L17
+    (,pantone193-fill (53 205 115 189 116 198 56 213 53 205))
+    ;; L18
+    (,mycolor1-fill (46 207 53 205 24 86 17 88 46 207))
+    ;; L19
+    (,mycolor1-fill (24 86 22 77 83 62 85 70 24 86) )))
 
 
-
-(defparameter polylinelist NIL)
+(defparameter *polyline-list* NIL)
 
 (defun create-logo (agg)
   (let ((cnt 0))
-    (setq polylinelist NIL)
-    (dolist (i new-point-list)
+    (setq *polyline-list* NIL)
+    (dolist (i *new-point-list*)
       (let ((color (car i))
 	    pl)
 	(incf cnt)
@@ -169,17 +149,17 @@
 				       (if (eq color opal:white-fill)
 					   opal:white-fill
 					   NIL)))))
-	(push pl polylinelist)
+	(push pl *polyline-list*)
 	(opal:add-component agg pl)))
-    (setq polylinelist (nreverse polylinelist))))
+    (setq *polyline-list* (nreverse *polyline-list*))))
 
 (defun shrink-logo (amount numtimes win)
   (let ((inc (/ (- 1 (/ 1.0 amount)) numtimes)))
     (dotimes (i numtimes)
-      (let ((npl new-point-list)
+      (let ((npl *new-point-list*)
 	    (thisinc (- 1.0 (* inc (1+ i))))
 	    thispl)
-	(dolist (pl polylinelist)
+	(dolist (pl *polyline-list*)
 	  (setq thispl (car npl))
 	  (setq npl (cdr npl))
 	  (let ((l nil))
@@ -190,10 +170,10 @@
       (opal:update win))))
 
 (defun reset-logo-size ()
-  (dolist (pl polylinelist)
+  (dolist (pl *polyline-list*)
     (s-value pl :point-list (g-value pl :copy-point-list))))
 
-(defparameter top-agg NIL)
+(defparameter *top-agg* nil)
 (defparameter font (opal:get-standard-font NIL :bold :very-large))
 
 (defparameter white-to-black NIL)
@@ -208,25 +188,25 @@
     (0.52 1.00 0.02) (0.83 1.00 0.00) (1.00 0.81 0.00) (1.00 0.48 0.00)
     (1.00 0.07 0.00)))
 
-(defparameter GarnetStringList
+(defparameter *garnet-string-list*
   '("Generating an" "Amalgam of" "Real-time," "Novel" "Editors and" "Toolkits"))
 
-(defparameter FirstLetterList NIL)
-(defparameter StringList NIL)
-(defparameter FirstLetterObjs NIL)
+(defparameter *first-letter-list* '())
+(defparameter *string-list* '())
+(defparameter *first-letter-objs* '())
 (defparameter FirstLetterOffset 0)
 (defparameter maxwh 0)
-(defparameter objs-list NIL)
+(defparameter *objs-list* '())
 
-(defparameter char-origin-x 35)
-(defparameter char-origin-y 60)
+(defparameter *char-origin-x* 35)
+(defparameter *char-origin-y* 60)
 
-(defun Create-Lists (strings)
-  (setq FirstLetterList NIL)
-  (setq StringList NIL)
+(defun create-lists (strings)
+  (setq *first-letter-list* '())
+  (setq *string-list* '())
   (dolist (s (reverse strings))
-    (push (subseq s 0 1) FirstLetterList)
-    (push (subseq s 1) StringList)))
+    (push (subseq s 0 1) *first-letter-list*)
+    (push (subseq s 1) *string-list*)))
 
 
 (defun Create-Color-List (numcolors)
@@ -257,8 +237,8 @@
 
 (defun Create-String-Objs (init-x init-y font agg)
   (let ((cur-y init-y)
-	objs-list obj)
-    (dolist (s StringList)
+	*objs-list* obj)
+    (dolist (s *string-list*)
       (setq obj
 	    (create-instance NIL opal:text (:string s)
 			     (:left (+ FirstLetterOffset init-x))
@@ -268,14 +248,14 @@
                              (:fast-redraw-p :redraw)
                              (:fast-redraw-line-style opal:white-line)))
       (opal:add-component agg obj)
-      (push obj objs-list)
+      (push obj *objs-list*)
       (incf cur-y maxwh))
-    (Reverse objs-list)))
+    (Reverse *objs-list*)))
 
-(defun Create-First-Letter-Objs (init-x init-y font agg)
+(defun create-first-letter-objs (init-x init-y font agg)
   (let ((maxw 0) (cur-x init-x)
-	w h objs-list obj)
-    (dolist (s FirstLetterList)
+	w h *objs-list* obj)
+    (dolist (s *first-letter-list*)
       (setq obj
 	    (create-instance NIL opal:text (:string s)
 			     (:left 0) ; left is set below
@@ -290,49 +270,50 @@
       (when (> w maxw)
 	(setq maxw w))
       (opal:add-component agg obj)
-      (push obj objs-list))
-    (setq objs-list (reverse objs-list))
+      (push obj *objs-list*))
+    (setq *objs-list* (reverse *objs-list*))
     ; now set the lefts
-    (dolist (obj objs-list)
+    (dolist (obj *objs-list*)
       (s-value obj :left cur-x)
       (s-value obj :initial-x cur-x)
       (s-value obj :initial-y init-y)
       (incf cur-x maxwh))
-    (setq FirstLetterObjs objs-list)
+    (setq *first-letter-objs* *objs-list*)
     (setq FirstLetterOffset (+ 2 maxw))))
 
 
 
 (defparameter Fader NIL)
-(defparameter win NIL)
+(defparameter *win* nil)
 ;;; status cycles between :beginning, :little and :letters-are-down
-(defparameter status :beginning)
+(defparameter *status* :beginning)
 
 ;;; Restore to :beginning
-(defun Reset ()
-  (dolist (obj FirstLetterObjs)
+(defun reset ()
+  (dolist (obj *first-letter-objs*)
     (s-value obj :left (g-value obj :initial-x))
     (s-value obj :top (g-value obj :initial-y))
     (s-value obj :visible NIL))
-  (dolist (obj objs-list)
+  (dolist (obj *objs-list*)
     (s-value obj :visible NIL))
   (reset-logo-size)
-  (opal:update win)
-  (setq status :beginning))
+  (opal:update *win*)
+  (setq *status* :beginning))
 
 (defun Circle-Down (numtimes)
   (let ((inc (/ PI 2 (1- numtimes)))
 	(angle 0)
 	offset)
     (dotimes (i numtimes)
+      (sleep 0.1)
       (setq offset 0)
-      (dolist (obj FirstLetterObjs)
+      (dolist (obj *first-letter-objs*)
 	(s-value obj :top
-		 (+ char-origin-y (Round (* offset (Sin angle)))))
+		 (+ *char-origin-y* (Round (* offset (Sin angle)))))
 	(s-value obj :left
-		 (+ char-origin-x (Round (* offset (Cos angle)))))
+		 (+ *char-origin-x* (Round (* offset (Cos angle)))))
 	(incf offset maxwh))
-      (opal:update win)
+      (opal:update *win*)
       (if (= i (- numtimes 2))
 	  ; then next time will be last
 	  (setq angle (/ PI 2))
@@ -340,7 +321,7 @@
 
 (defun DoFade ()
   (s-value fader :visible T)
-  (dolist (lin objs-list)
+  (dolist (lin *objs-list*)
     (s-value fader :left (g-value lin :left))
     (s-value fader :top (g-value lin :top))
     (s-value fader :width (g-value lin :width))
@@ -348,9 +329,9 @@
     (s-value lin :visible T)
     (dolist (color white-to-black)
       (s-value fader :filling-style color)
-      (opal:update win)))
+      (opal:update *win*)))
   (s-value fader :visible NIL)
-  (opal:update win))
+  (opal:update *win*))
 
 (defun DoColors (obj)
   (s-value obj :visible T)
@@ -358,81 +339,90 @@
     ;; Trick update into just drawing the object again (during the erase phase)
     (s-value obj :fast-redraw-line-style (g-value obj :line-style))
     (s-value obj :line-style color)
-    (opal:update win)
+    (opal:update *win*)
     (sleep #+hpux .08 #-hpux 0.01))
   (s-value obj :fast-redraw-line-style opal:white-line)
   ;(s-value obj :line-style opal:default-line-style)
-  (opal:update win))
+  (opal:update *win*))
 
-(defun Go-To-Next-Status ()
-  (case status
-    (:beginning (shrink-logo 2 5 win)
-		(dolist (obj FirstLetterObjs)
+(defun go-to-next-status ()
+  (case *status*
+    (:beginning (shrink-logo 2 5 *win*)
+		(dolist (obj *first-letter-objs*)
 		  (s-value obj :visible T))
-		(opal:update win)
-		(setf status :little))
+		(opal:update *win*)
+		(setf *status* :little))
     (:little
      (Circle-Down 30)
      (if (g-value opal:color :color-p)
-	 (dolist (obj objs-list)
+	 (dolist (obj *objs-list*)
 	   (DoColors obj))
 	 (DoFade))
-     (setf status :letters-are-down))
+     (setf *status* :letters-are-down))
     (:letters-are-down (reset))
-    (T (error "status bad ~s" status))))
+    (T (error "status bad ~s" *status*))))
 
 (defun Re-Animate ()
   (loop
    (go-to-next-status)
-   (if (eq status :letters-are-down)
+   (if (eq *status* :letters-are-down)
      (return-from re-animate)
      (sleep 1))))
 
-(defun Do-Go (&key (strings GarnetStringList)
+(defun do-go (&key (strings *garnet-string-list*)
 		   (numFades 17)
 		   dont-enter-main-event-loop
-		   (double-buffered-p T))
-  (Create-Lists strings) ; convert string list into appropriate form
-  (setq win (create-instance NIL inter:interactor-window
+		(double-buffered-p T))
+  ;; Convert string list into appropriate form
+  (create-lists strings)
+  (setq *top-agg* (create-instance NIL opal:aggregate))
+  ;; Expand to fit contexnts of aggregate. Never shrink.
+  (setq *win* (create-instance nil inter:interactor-window
 		      (:title "Garnet Logo")
-		      (:left 0)(:top 0)
-		      (:width 270)(:height 235)
+		      (:left 0)
+		      (:top 0)
+		      (:width (o-formula (max (+ (or (gv *top-agg* :width) 0) 6)
+					      (or (gv :self :width) 270))))
+		      (:height (o-formula (max (+ (or (gv *top-agg* :height) 0) 6)
+					       (or (gv-local :self :height) 235))))
 		      (:double-buffered-p double-buffered-p)
-		      (:aggregate (setq top-agg
-					(create-instance NIL opal:aggregate)))))
-  (Create-Logo top-agg)
-  (opal:update win)
+		      (:aggregate *top-agg*)))
+  (Create-Logo *top-agg*)
+  (opal:update *win*)
   (Create-Color-List numFades)
   (sleep 3)
-  (setq objs-list nil)       ;; needed if you stop
-  (setq FirstLetterObjs nil) ;; and restart demo-logo.
-  (Go-To-Next-Status) ;; will shrink
-  (Create-First-Letter-Objs char-origin-x char-origin-y font top-agg)
-  (setq objs-list (create-string-objs char-origin-x char-origin-y
-				      font top-agg))
+  ;; needed if you stop
+  (setq *objs-list* '())
+  ;; and restart demo-logo.
+  (setq *first-letter-objs* '())
+  ;; will shrink
+  (go-to-next-status)
+  (create-first-letter-objs *char-origin-x* *char-origin-y* font *top-agg*)
+  (setq *objs-list* (create-string-objs *char-origin-x* *char-origin-y*
+				      font *top-agg*))
   (unless (g-value opal:color :color-p)
     (create-instance 'Fader Opal:Rectangle
 		     (:visible NIL)
 		     (:filling-style NIL)(:Line-style NIL)
 		     (:draw-function :AND))
-    (opal:add-component top-agg fader))
-  (opal:update win)
+    (opal:add-component *top-agg* fader))
+  (opal:update *win*)
   (sleep 2)
-  (Go-To-Next-Status) ;; will circle-down and blink
+  (go-to-next-status) ;; will circle-down and blink
   (create-instance 'starter inter:button-interactor
-		   (:start-where `(:in ,win))
+		   (:start-where `(:in ,*win*))
 		   (:start-event '(:double-leftdown))
 ;;		    '(:any-keyboard :any-mousedown))
-		   (:window win)
-		   (:continuous NIL)
+		   (:window *win*)
+		   (:continuous nil)
 		   (:final-function
 		    #'(lambda (&rest args)
 			(declare (ignore args))
-			(Go-To-Next-Status))))
+			(go-to-next-status))))
   "Press in window to start, press again to reset"
   (unless dont-enter-main-event-loop
       (inter:main-event-loop)))
 
 (defun Do-Stop ()
-  (setf status :beginning)
-  (opal:destroy win))
+  (setf *status* :beginning)
+  (opal:destroy *win*))
